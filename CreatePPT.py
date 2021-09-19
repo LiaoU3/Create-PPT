c84d0af178ed7565a3e1ad5554323418e616a972
LiaoU3 <vincent932693@gmail.com>
2021-09-19 14:43:30 +0800

    Regular Update

--- CreatePPT.py
+++ CreatePPT.py
@@ -1,10 +1,11 @@
-# 2021/09/10 version 1.1
+# 2021/09/13 version 1.2
 # Author : Vincent
 
 from pptx import Presentation
 import datetime
 import os
 
+# get the current n'th week of the month
 def get_week_of_month():
     today = datetime.datetime.now()
     begin = int(datetime.date(today.year, today.month, 1).strftime("%W"))
@@ -15,32 +16,35 @@ def get_week_of_month():
 def main():
     # set your name here
     name = 'Vincent'
+
     # you will get the correct weekday, if you create file on Monday(which is the next monday)
-    if datetime.datetime.today().weekday() == 1 :
+    if datetime.datetime.today().weekday() == 0 :
         week = get_week_of_month() - 1
     else:
         week = get_week_of_month()
 
+    # set the correct file name according to the current date
     file_name = str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month) + 'M-' +str(week) + 'W-週報'
 
+    # check if the file is already exist
+    keep_going = 'y'
     if os.path.isfile(file_name + '.pptx'):
         keep_going = input('The file is already exist, do you want to delete it and create a new file ? (y/n) ')
     if keep_going == 'y':
         # Create pptx object
-        prs = Presentation('src/2020-4M-1W-週報.pptx')
-
-
+        prs = Presentation('src/Template.pptx')
         # Set the title of first page 
         title = prs.slides[0].shapes.title
         title.text = file_name
         # Set the subtitle of first page 
         subtitle = prs.slides[0].placeholders[1]
         subtitle.text = name
-
         prs.save(file_name + '.pptx')
         print('Sate : File Created -> ' + file_name + '.pptx')
     else:
         print("State : Didn't do anything")
+
+    input("Press any key to exit...")
 
 if __name__ == '__main__':
     main()
--- README.txt
+++ README.txt
@@ -1,14 +1,20 @@
-這個 CreatePPT.py 可以依據創建檔案的時間，自動製作出匹配日期的pptx檔
+Introduce :
+  這個 CreatePPT.py 可以依據創建檔案的時間，自動製作出匹配日期的pptx檔
+  你也可以更改在src資料夾中的Template.pptx模板，來符合你的需求
 
-1.請先使用下方指令安裝pptx函式庫
-  -> pip install pptx
+Usage :
+  1.請先使用下方指令安裝pptx函式庫
+    -> pip install pptx
 
-2.進入檔案，找到變數name，並打上自己的名字
+  2.進入CreatPPT.py，找到變數name(第18行)，並改成自己的名字
 
+  3.執行
 
 Current Version : 1.1
 Author          : Vincent
 
 log:
-2021/09/10 V1.0 創建初始程式
-2021/09/10 V1.1 加入防止誤刪檢查
+  2021/09/10 V1.0 創建初始程式
+  2021/09/10 V1.1 加入防止誤刪檢查
+  2021/09/13 V1.2 修正在星期一不能正確顯示weekday的錯誤並更新README.txt
+  2021/09/15 V1.3 更改模板PPT的名稱，更新README.txt
